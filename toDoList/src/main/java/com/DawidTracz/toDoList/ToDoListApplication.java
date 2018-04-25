e856d7c34ce2df46cf92b0159c91b87a11ee07d0
dawid.tracz@o2.pl <dawid,tracz@o2.pl>
2018-04-25 13:35:26 +0200

    first commit with intellij

--- toDoList/src/main/java/com/DawidTracz/toDoList/ToDoListApplication.java
+++ toDoList/src/main/java/com/DawidTracz/toDoList/ToDoListApplication.java
@@ -1,8 +1,11 @@
 package com.DawidTracz.toDoList;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ToDoListApplication  extends SpringBootServletInitializer {
@@ -10,4 +13,12 @@ public class ToDoListApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
+
+	@Bean
+	public ServletRegistrationBean h2servletRegistration(){
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
+	}
+
 }
